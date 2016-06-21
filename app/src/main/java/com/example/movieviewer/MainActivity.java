038705440856fe53634c529535f96d7a63fe60c8
james <jtmaher2@gmail.com>
2016-06-20 19:35:58 -0500

    removed FAB

--- app/src/main/java/com/example/movieviewer/MainActivity.java
+++ app/src/main/java/com/example/movieviewer/MainActivity.java
@@ -5,19 +5,13 @@ package com.example.movieviewer;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.Switch;
 
 import org.json.JSONArray;
@@ -54,38 +48,26 @@ public class MainActivity extends AppCompatActivity {
         movieArrayAdapter = new MovieArrayAdapter(this, movieList);
         movieGridView.setAdapter(movieArrayAdapter);
 
-        // configure FAB to hide keyboard and initiate web service request
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // configure switch to toggle between popular and top-rated
+        final Switch popularOrTop = (Switch) findViewById(R.id.popularOrTop);
 
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // get text from titleEditText and create web service URL
-                    final Switch popularOrTop = (Switch) findViewById(R.id.popularOrTop);
+        if (popularOrTop != null) {
+            popularOrTop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    URL url;
 
+                    if (isChecked)
+                        url = createURL(popularOrTop.getTextOn().toString());
+                    else
+                        url = createURL(popularOrTop.getTextOff().toString());
 
-
-                    if (popularOrTop != null) {
-                        popularOrTop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                                URL url;
-
-                                if (isChecked)
-                                    url = createURL(popularOrTop.getTextOn().toString());
-                                else
-                                    url = createURL(popularOrTop.getTextOff().toString());
-
-                                // hide keyboard and initiate a GetMovieTask to download movie data from
-                                // TheMovieDB.org in a separate thread
-                                if (url != null) {
-                                    GetMovieTask getMovieTask = new GetMovieTask();
-                                    getMovieTask.execute(url);
-                                } else {
-                                    Snackbar.make(findViewById(R.id.coordinatorLayout), R.string.invalid_url, Snackbar.LENGTH_LONG).show();
-                                }
-                            }
-                        });
+                    // hide keyboard and initiate a GetMovieTask to download movie data from
+                    // TheMovieDB.org in a separate thread
+                    if (url != null) {
+                        GetMovieTask getMovieTask = new GetMovieTask();
+                        getMovieTask.execute(url);
+                    } else {
+                        Snackbar.make(findViewById(R.id.coordinatorLayout), R.string.invalid_url, Snackbar.LENGTH_LONG).show();
                     }
                 }
             });
--- app/src/main/java/com/example/movieviewer/MovieArrayAdapter.java
+++ app/src/main/java/com/example/movieviewer/MovieArrayAdapter.java
@@ -6,13 +6,11 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
